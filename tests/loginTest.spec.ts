0fe152e31df056a4a369f2e766c86a0b62c92491
ZehraGormus <sayilarla.34@gmail.com>
2024-10-24 18:05:07 +0200

    secon commit

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -9,12 +9,6 @@ test('test', async ({ page }) => {
     await Login.goToLoginPage()
     await Login.login('tomsmith', 'SuperSecretPassword!')
 
-    // await page.goto('https://the-internet.herokuapp.com/login');
-    // await page.getByLabel('Username').click();
-    // await page.getByLabel('Username').fill('tomsmith');
-    // await page.getByLabel('Password').click();
-    // await page.getByLabel('Password').fill('SuperSecretPassword8');
-    // await page.getByRole('button', { name: ' Login' }).click();
+    
 });
 
-//test
